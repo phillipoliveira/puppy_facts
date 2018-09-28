80fe5a3a6135be62f5e9dc610a0866522dfff0f5
Phillip Oliveira <phillip.j.oliveira@gmail.com>
2018-09-28 10:40:56 -0400

    commit

--- src/listener.py
+++ src/listener.py
@@ -23,6 +23,7 @@ def inbound():
             event_data.get("challenge"), 200, {"content_type": "application/json"}
            )
     elif "event" in event_data:
+        print(event_data)
         if all ([(event_data['event']['type'] == 'member_joined_channel'), (event_data['event']['user'] == 'UCZDTNS80')]):
             Distributor.add_distributor(type="slack", slack_channel_id=event_data['event']['channel'])
         elif all ([(event_data['event']['type'] == 'member_left_channel'), (event_data['event']['user'] == 'UCZDTNS80')]):
